feab632494a29675afef6d2d0f3b5503a2d38f19
SoohoLee <hyeri0603@naver.com>
2023-12-01 23:38:18 +0900

    인터페이스 분리 231201

---
+++ src/test/java/hello/jdbc/exception/translator/ExTranslatorV1Test.java
@@ -0,0 +1,94 @@
+package hello.jdbc.exception.translator;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.ex.MyDbException;
+import hello.jdbc.repository.ex.MyDuplicateKetException;
+import hello.jdbc.util.SupportTestWithSpring;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Random;
+
+import static hello.jdbc.connection.ConnectionsConst.*;
+
+@Slf4j
+public class ExTranslatorV1Test extends SupportTestWithSpring {
+
+    Repository repository;
+    Service service;
+
+    @BeforeEach
+    void setUp() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new Repository(dataSource);
+        service = new Service(repository);
+    }
+
+    @DisplayName("키 중복 저장")
+    @Test
+    void duplicateKeySave() throws Exception {
+        service.create("MyId");
+        service.create("MyId");
+    }
+
+    @RequiredArgsConstructor
+    static class Service {
+        private final Repository repository;
+
+        public void create(String memberId) {
+            try {
+                repository.save(new Member(memberId, 0));
+                log.info("savedId={}", memberId);
+            } catch (MyDuplicateKetException e) {
+                log.error("키 중복, 복구 시도");
+                String retryId = generateNewId(memberId);
+                log.info("retryId={}", retryId);
+                repository.save(new Member(retryId, 0));
+            } catch (MyDbException e) {
+                log.error("데이터 접근 계층 예외 발생, 복구 불가");
+                throw e;
+            }
+        }
+
+        private String generateNewId(String memberId) {
+            return memberId + "_" + new Random().nextInt(10000);
+        }
+    }
+
+    @RequiredArgsConstructor
+    static class Repository {
+        private final DataSource dataSource;
+
+        public Member save(Member member) {
+            String sql = "insert into member(member_id, money) values (?, ?)";
+            Connection con = null;
+            PreparedStatement pstmt = null;
+
+            try {
+                con = dataSource.getConnection();
+                pstmt = con.prepareStatement(sql);
+                pstmt.setString(1, member.getMemberId());
+                pstmt.setInt(2, member.getMoney());
+                pstmt.executeUpdate();
+                return member;
+            } catch (SQLException e) {
+                if (e.getErrorCode() == 23505) {
+                    throw new MyDuplicateKetException(e);
+                }
+                throw new MyDbException(e);
+            } finally {
+                JdbcUtils.closeStatement(pstmt);
+                JdbcUtils.closeConnection(con);
+            }
+        }
+    }
+}
